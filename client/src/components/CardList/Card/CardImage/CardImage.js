93dcbcf85323daa113a0b7215454eb3dac186726
Roderick Romero <rjrr507@gmail.com>
2020-05-24 11:37:17 -0500

    images dummy assets

--- client/src/components/CardList/Card/CardImage/CardImage.js
+++ client/src/components/CardList/Card/CardImage/CardImage.js
@@ -2,11 +2,11 @@ import React from 'react';
 // import './../../style.css';
 import PropTypes from 'prop-types';
 // const loli = './../../../img/';
-const loli = require ('./../../../../img/loli2.jpg');
+const hat = require ('../../../../assets/img/ORH82L0.jpg');
 
 const CardImage = ({img})=>(
     <div  className="background">       
-        <img src={loli} alt="Loli.jpg" className = 'petImage'/>
+        <img src={hat} alt="Loli.jpg" className = 'petImage'/>
     </div>
     
 );
--- client/src/components/CardList/index.js
+++ client/src/components/CardList/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card/index';
 // import './style.css';
-import Rodal from 'rodal';
 import 'rodal/lib/rodal.css';
 const loli = require('./../../../src/img/loli2.jpg');
 
