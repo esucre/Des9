fc2a5e839caec2a05af76684e083d7f1c71a4f8f
emilio sucre <emilio.sucre@utp.ac.pa>
2020-05-24 14:07:58 -0500

    Se crea el componente para el separador

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardList from '../CardList/index'
+import Separador from './../Separador/Separador'
 import PropTypes from 'prop-types';
 // import './../CardList/style.css';
 
@@ -7,7 +8,7 @@ const News = ()=>(
   
     <div className="seccion">
         <h2 className ='text-center tamanoh2'>Noticias</h2>
-   
+        <Separador></Separador>
     <CardList></CardList>
         
     </div>
---
+++ client/src/components/Separador/Separador.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import separador from './../../img/Separador-oficial.png'
+
+
+const Separador = ()=>(
+  
+    <div >
+        <img className="separador" src = {separador}></img>   
+    </div>
+
+
+);
+
+export default Separador;
