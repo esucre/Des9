3e57bc9ab073d877894b4c8761138317fe10ac33
emilio sucre <emilio.sucre@utp.ac.pa>
2020-06-16 13:56:58 -0500

    Correcion de ruta

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -7,7 +7,7 @@ import NavBar from '../NavBarPrueba/NavBarPrueba';
 export default function Switcher() {
   return (
     <Switch>
-      <Route path="/" exact component={Home}></Route>
+      <Route path="/index.html" exact component={Home}></Route>
       <Route path="/Merch" exact component={Merch}></Route>
       <Route path="/Login" exact component={Login}></Route>
       <Route path="/PanelUsuario" exact component={PanelUsuario}></Route>
